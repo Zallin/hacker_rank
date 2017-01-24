9b167b664dfa59947934dc21f37928fa6af396b5
Tim Zallin <tim.zallin@gmail.com>
2017-01-24 20:57:10 +0300

    minor test changes

--- test/hacker_rank/convex_hull_test.clj
+++ test/hacker_rank/convex_hull_test.clj
@@ -54,6 +54,6 @@
   (let [test-data [[1 1] [1 2] [1 3] [1 4]
                    [3 1] [3 2] [3 3] [3 4]
                    [5 1] [5 2] [5 3] [5 4]]]
-    (is (= (convex-hull test-data)
+    (is (= (convex-hull-points-set test-data)
            #{[1 1] [5 1] [5 4] [1 4]}))))
                  
